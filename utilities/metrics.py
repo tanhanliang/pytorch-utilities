4aaf0b279eafbf21faae74e0319004ff6f58e4a9
tanhanliang <tanhanliang@outlook.com>
2019-03-31 21:55:57 +0100

    Better implementation of accuracy function

--- utilities/metrics.py
+++ utilities/metrics.py
@@ -5,18 +5,19 @@ Calculate common metrics such as accuracy.
 
 def accuracy(outputs, targets):
     """
-    Calculates the accuracy of some predictions.
-
-    Use this if the network outputs a simple probability for a two-class 
-    classification problem.
+    Calculates the accuracy of some predictions. Inputs are one hot encoded.
 
     @param outputs: the outputs of the network as a torch.Tensor
     @param targets: what you want the network to output, as a torch.Tensor
     @return: the accuracy, a single number
     """
 
-    outputs_len = outputs.size()[0]
-    return ((outputs > 0.5) == targets.byte()).sum().item()/outputs_len
+    length = outputs.size()[0]
+    classes = len(outputs[0])
+    predictions = torch.argmax(outputs, 1)
+    targets_single_number = torch.argmax(targets, 1)
+
+    return (predictions == targets_single_number).sum().item()/length
 
 
 def precision(outputs, targets):
